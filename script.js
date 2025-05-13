7adfd7ea64d4fc07d5d0a4b32003b99a2c619363
Vinícius Santiago <31862989+viniciussnx@users.noreply.github.com>
2025-05-13 11:14:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function abrirCarta() {
+  const senha = document.getElementById("senha").value.trim().toLowerCase();
+  const envelope = document.getElementById("envelope");
+  const erro = document.getElementById("mensagemErro");
+  const senhaContainer = document.getElementById("containerSenha");
+
+  if (senha === "misto quente") {
+    envelope.classList.add("open");
+    envelope.classList.add("expandido");
+    erro.textContent = "";
+    senhaContainer.style.display = "none";
+  } else {
+    erro.innerHTML = 'Senha incorreta. Tente novamente.<br><strong>Dica:</strong> O prato que você fez que Vinícius ama';
+  }
+}
